2115e2949d7f87a82a659dfb700d5ba7c96683cb
beadk <bjarke@ejsensie.dk>
2015-11-08 20:47:49 +0100

    Flies added

---
+++ src/DTO/FieldsDTO.java
@@ -0,0 +1,17 @@
+package DTO;
+
+public class FieldsDTO {
+	private String fields;
+
+	public FieldsDTO(String fields) {
+		this.fields = fields;
+	}
+
+	public String getFields() {
+		return fields;
+	}
+
+	public void setFields(String fields) {
+		this.fields = fields;
+	}
+}
